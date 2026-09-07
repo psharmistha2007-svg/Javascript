4cc9a1d7dfa37e4c99f315cc5bdb670552ecef1c
Sharmistha Paul <psharmistha2007-svg@gmail.com>
2026-09-07 22:27:21 +0530

    Variables

---
+++ js/1_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456789
+let accountEmail = "sharmistha@gmail.com"
+var accountPassword = "123"
+accountcity = "Kolkata"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "sh@sh.com"
+accountPassword = "12345"
+accountcity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPnassword, accountcity, accountState]);
