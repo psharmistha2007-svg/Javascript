3137ea8e759c8a86faddc5e32acbc4f492bb7c40
Sharmistha Paul <psharmistha2007-svg@gmail.com>
2026-09-09 19:54:01 +0530

    Data

---
+++ js/2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; 
+
+// alert(3 + 3)
+
+console.log(3 
+    + 
+    3)
+
+console.log("Sharmistha")
+
+let name = "Sharmistha"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "Sharmistha"); //string
+console.log(typeof age); //number
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
