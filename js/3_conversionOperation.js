1ff52705c5689aa1c10016ce9ae47d0b9d41b644
Sharmistha Paul <psharmistha2007-svg@gmail.com>
2026-09-09 21:07:57 +0530

    Conversion

---
+++ js/3_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = 33
+console.log(typeof score); //number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //number
+let isLoggedIn = "sharmistha"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn); //boolean
+console.log(booleanIsLoggedIn); //boolean
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); //string
+console.log(stringNumber); //string
+
+// Operations
+let value = 3
+let negValue = -value
+console.log(negValue); //-3
+console.log(2 + 2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2**3); //8
+console.log(2/2); //1
+console.log(2%3); //2
+
+let str1 = "Hello"
+let str2 = "Sharmistha"
+let str3 = str1 + str2
+console.log(str3); //HelloSharmistha
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+console.log((3 + 4) * 5 % 3); //2
+console.log(+true); //1
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); //101
